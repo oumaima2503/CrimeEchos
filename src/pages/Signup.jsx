ace4cdc92a05511b0aef1a94e84958ed99a28036
Ikram <ikramassiri9@gmail.com>
2024-12-04 14:53:08 +0100

    signup retouche

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import blood from '../assets/blood.svg';
 import arriere from '../assets/arriere.png';
+import arr from '../assets/arrieresansblood.svg'
 import logo from '../assets/logo_crime.png';
 
 function SignUpPage() {
@@ -13,10 +14,10 @@ function SignUpPage() {
   alt="Blood Icon" 
 />
       {/* Left Side: Form */}
-      <div className="w-full md:w-1/2  relative flex items-center justify-center koulen "> 
-      <div className="w-full md:w-1/2 bg-white p-6 rounded-lg flex flex-col justify-center border border-white rounded-3xl backdrop-blur-sm bg-opacity-40 ml-10 mr-10">     
+      <div className="w-full md:w-1/2  relative flex items-center justify-center  "> 
+      <div className="w-full h-[500px] bg-white p-6 flex flex-col justify-center border border-white rounded-3xl backdrop-blur-sm bg-opacity-40 ml-10 mr-10">     
            <h1 className="text-3xl font-bold mb-4 text-center koulen">CREATE ACCOUNT</h1>
-          <p className="text-sm text-center text-white mb-6">
+          <p className="text-lg text-center text-white mb-6">
             Already have an Account?{" "}
             <a href="/login" className="text-yellow-400 hover:underline">
               Log in
@@ -24,68 +25,63 @@ function SignUpPage() {
           </p>
           <form className="space-y-4">
             <div>
-              <label className="block text-sm font-medium mb-1">Name</label>
+              <label className="block text-lg font-medium ">Name</label>
               <input
-                type="text"
-                className="w-full px-3 py-2 bg-gray-700 rounded-md outline-none focus:ring-2 focus:ring-yellow-400"
-                placeholder="Enter your name"
-              />
+  type="text"
+  className="w-full px-3 py-1 bg-transparent border-b-2 border-white focus:outline-none focus:border-yellow-400"
+
+/>
+
             </div>
             <div>
-              <label className="block text-sm font-medium mb-1">Email</label>
+              <label className="block text-lg font-medium ">Email</label>
               <input
-                type="email"
-                className="w-full px-3 py-2 bg-gray-700 rounded-md outline-none focus:ring-2 focus:ring-yellow-400"
-                placeholder="Enter your email"
-              />
+  type="text"
+  className="w-full px-3 py-1 bg-transparent border-b-2 border-white focus:outline-none focus:border-yellow-400"
+
+/>
+
             </div>
             <div>
-              <label className="block text-sm font-medium mb-1">Password</label>
+              <label className="block text-lg font-medium ">Password</label>
               <input
-                type="password"
-                className="w-full px-3 py-2 bg-gray-700 rounded-md outline-none focus:ring-2 focus:ring-yellow-400"
-                placeholder="Enter your password"
-              />
+  type="text"
+  className="w-full px-3 py-1 bg-transparent border-b-2 border-white focus:outline-none focus:border-yellow-400"
+
+/>
+
             </div>
+            <div className="flex justify-center">
             <button
               type="submit"
-              className="w- bg-[#982222] text-white py-2 rounded-md font-bold hover:bg-red-700 "
-            >
+              className="w-24 bg-[#982222] text-white py-2 rounded-md font-bold hover:bg-red-700 text-2xl koulen">
               SIGN UP
-            </button>
+            </button></div>
           </form>
         </div>
         </div>
-        {/* Right Side: Logo and Decoration */}
-        <div className="w-full md:w-1/2  bg-cover bg-center relative h-screen ">
-          <div className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-0 p-6">
-            <h1 className="text-4xl font-bold mb-4"></h1>
-            
-            <h1 className="text-white text-3xl md:text-4xl font-bold z-10 -mt-40 nosifer">GET STARTED</h1>
-            <h1 className="text-[#580B0B] text-3xl md:text-4xl font-bold z-0 nosifer -mt-[30px] md:-mt-[30px]">
-            GET STARTED
-          </h1>
-            <img 
-  className="w-32 h- mb-2" 
-  src={logo} 
-  alt="logo " 
-/>  
-            <p className="text-3xl text-white uppercase font-bold tracking-wide koulen">
-              Crimechos
-            </p>
-          </div>
-          
-          {/* Background (Crime Scene Tape) */}
+  {/* Right Side: Logo and Decoration */}
+<div className="w-full md:w-1/2 bg-cover bg-right relative h-screen">
+  <div className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-0 p-6">
+    <h1 className="text-4xl font-bold mb-4"></h1>
 
-          <img 
-            src={arriere}
-            alt="Crime Scene Background"
-            className=" w-full h-full rounded-3xl "
-          />
-        </div>
-    
+    <h1 className="text-white text-3xl md:text-4xl font-bold z-10 -mt-40 nosifer">GET STARTED</h1>
+    <h1 className="text-[#580B0B] text-3xl md:text-4xl font-bold z-0 nosifer -mt-[30px] md:-mt-[30px]">
+      GET STARTED
+    </h1>
+    <img className="w-48 h-auto mb-2 mt-6" src={logo} alt="logo" />
+    <p className="text-3xl text-white uppercase font-bold tracking-wide koulen">Crimechos</p>
+  </div>
+
+  {/* Background (Crime Scene Tape) */}
+  <img 
+    src={arr} 
+    alt="Crime Scene Background"
+    className="w-full h-full  object-cover rounded-3xl "
+  />
+</div>
+
     </div>
   );
 }
-
 export default SignUpPage;
