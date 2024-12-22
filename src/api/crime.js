eb6554be4a87971161c15a0e570042c5d98791ea
Ikram <ikramassiri9@gmail.com>
2024-12-23 00:46:40 +0100

    vroujou3 ila lwara2

--- src/api/crime.js
+++
@@ -1,9 +0,0 @@
-import { crimes } from './src/api/crimeData.js';
-
-export default function handler(req, res) {
-  if (req.method === 'GET') {
-    res.status(200).json(crimes);
-  } else {
-    res.status(405).json({ message: 'Method not allowed' });
-  }
-}
--- src/api/force.js
+++
@@ -1,9 +0,0 @@
-import { force } from './src/api/forceData.js';
-
-export default function handler(req, res) {
-  if (req.method === 'GET') {
-    res.status(200).json(force);
-  } else {
-    res.status(405).json({ message: 'Method not allowed' });
-  }
-}
